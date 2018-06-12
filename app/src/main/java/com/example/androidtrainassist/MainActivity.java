468adc2182301b067621c27ba8eef22a811af78b
lizhizhi <li540929968@163.com>
2018-06-12 21:04:38 +0800

    nothingImpotant
    nothingImpotant

--- app/src/main/java/com/example/androidtrainassist/MainActivity.java
+++ app/src/main/java/com/example/androidtrainassist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.androidtrainassist;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getShareFile(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/jpg");
+        startActivityForResult(intent, 101);
+    }
+
+    /**
+     * Dispatch incoming result to the correct fragment.
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Intent returnIntent = getIntent();
+        if (resultCode == RESULT_OK){
+            if (requestCode == 101){
+                Uri returnUri = returnIntent.getData();
+            }
+        }
+
     }
 }
